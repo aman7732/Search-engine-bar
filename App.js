107c874b9fe38c5bd7e96cec02937ca1bb5ab3b4
aman7732 <132009495+aman7732@users.noreply.github.com>
2024-01-19 22:51:34 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,5 @@
+const Search = () =>{
+    const Searchbox = document.getElementById("Search-item").Value.toUpperCase();
+    const Storeitem = document.getElementById("Product-list")
+    const Product = document.querySelectorAll(".Product")
+}
